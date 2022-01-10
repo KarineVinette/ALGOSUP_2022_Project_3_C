edbc91ea7497c6d3063ca1bf718f61d762567dc1
Rasputin-6 <78964703+BrendonDesvaux@users.noreply.github.com>
2022-01-10 15:43:51 +0100

    Console app v1
    Creation of the console app and added waves functions

---
+++ Synthe/Synthe.Tests/UnitTest1.fs
@@ -0,0 +1,13 @@
+﻿module Synthe.tests
+
+open NUnit.Framework
+open Undertone
+
+[<SetUp>]
+let Setup () =
+    ()
+
+[<Test>]
+let Wavesine() =
+    let result = Creation.sine 49.9
+    Assert.Equals(43.432432432342, result)
