6a2a78eee9d4bb91c85c234d41516e7a8356f468
Rasputin-6 <78964703+BrendonDesvaux@users.noreply.github.com>
2022-01-13 15:50:49 +0100

    Update Nunit Tests
    Commented waves xplot visualisation
    
    Modify test to make them pass with real values instead of null

--- Synthe/Waves.fs
+++ Synthe/Waves.fs
@@ -1,9 +1,4 @@
-<<<<<<< Updated upstream
-﻿module waves =
-=======
-﻿module waves 
-
->>>>>>> Stashed changes
+module waves 
     open System
     open XPlot.GoogleCharts
 
@@ -15,22 +10,17 @@
 
     let sinWave = Array.init limit (fun i -> amp * sin((freq/ Pi) * float i))
     
-    sinWave |> Chart.Line |> Chart.WithOptions(Options(curveType = "function")) |> Chart.Show
+    //sinWave |> Chart.Line |> Chart.WithOptions(Options(curveType = "function")) |> Chart.Show
 
     let triangle = Array.init limit (fun i ->  (2.*amp/Pi)* Math.Asin(sin((2.*Pi*float i)/freq)))
 
-    triangle |> Chart.Line |> Chart.WithOptions(Options(curveType = "function")) |> Chart.Show
+    //triangle |> Chart.Line |> Chart.WithOptions(Options(curveType = "function")) |> Chart.Show
 
     let squareWave = Array.init limit (fun i -> float(Math.Sign(sin(2. * Math.PI * float i*(freq/float limit)))))
 
-    squareWave |> Chart.Line |> Chart.Show
+    //squareWave |> Chart.Line |> Chart.Show
 
 
     let sawWave = Array.init limit (fun i -> (2.*amp/Pi)* Math.Atan(tan((2.*Pi*float i)/freq)))
 
-<<<<<<< Updated upstream
-    sawWave |> Chart.Line |> Chart.WithOptions(Options(curveType = "function")) |> Chart.Show
-=======
-    sawWave |> Chart.Line |> Chart.WithOptions(Options(curveType = "function")) |> Chart.Show
-
->>>>>>> Stashed changes
+    //sawWave |> Chart.Line |> Chart.WithOptions(Options(curveType = "function")) |> Chart.Show
