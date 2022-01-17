14c06e08cf98ff71fbc09b308f3a7feaaa74b096
Nicolas MIDA <nicolas.mida@algosup.com>
2022-01-17 16:52:23 +0100

    implement notes

--- Synthe/Waves.fs
+++ Synthe/Waves.fs
@@ -8,6 +8,36 @@ module waves
     let freq = 40.
     let limit = 500
 
+
+    type Note  =
+        | A
+        | A#
+        | B
+        | C
+        | C#
+        | D
+        | D#
+        | E
+        | F
+        | F#
+        | G
+        | G#
+
+    let NotesValues note  = 
+                match note with
+                | A -> 440.
+                | A# -> 466.16
+                | B -> 493.88
+                | C -> 261.63
+                | C# -> 277.18 
+                | D -> 293.66
+                | D# -> 311.13
+                | E -> 329.63
+                | F -> 349.23
+                | F# -> 369.99
+                | G -> 392.
+                | G# -> 415.31
+
     let sinWave = Array.init limit (fun i -> amp * sin((freq/ Pi) * float i))
     
     //sinWave |> Chart.Line |> Chart.WithOptions(Options(curveType = "function")) |> Chart.Show
