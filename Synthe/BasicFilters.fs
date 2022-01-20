0e4d973a27823cb851d3074e8ecf9e8c57811b83
Malo Archimbaud <malo.archimbaud@algosup.com>
2022-01-20 12:27:50 +0100

    enveloppe avec modif à faire

--- Synthe/BasicFilters.fs
+++ Synthe/BasicFilters.fs
@@ -22,33 +22,28 @@ module BasicFilters
 
     module enveloppe =
 
-
-        open System
-        open System.IO
+        open waves
         open XPlot.Plotly
 
         let sampleRate = 44100.
-        let Pi = Math.PI
-        let amplitude = 2.
-        let Frequency = 440.
         let limit = 44100
 
-        let enveloppe amplitude A D S R =
+        let enveloppe amplitude At De Su Re =
 
-            let Attack = Array.init (int (sampleRate*A)) (fun i -> amplitude/(sampleRate*A)* float i)
+            let Attack = Array.init (int (sampleRate*At)) (fun i -> amplitude/(sampleRate*At)* float i)
             
-            let Decay = Array.init (int (sampleRate*D)) (fun i -> amplitude - (amplitude-S*amplitude)/(sampleRate*D) *float i)
+            let Decay = Array.init (int (sampleRate*De)) (fun i -> amplitude - (amplitude-Su*amplitude)/(sampleRate*De) *float i)
 
-            let Sustain = Array.init (int (sampleRate - sampleRate*R - sampleRate*D - sampleRate*A)) (fun _ -> S*amplitude)
+            let Sustain = Array.init (int (sampleRate - sampleRate*Re - sampleRate*De - sampleRate*At)) (fun _ -> Su*amplitude)
 
-            let Release = Array.init (int (sampleRate*R)) (fun i -> S*amplitude - S*amplitude/(sampleRate*R) * float i)
+            let Release = Array.init (int (sampleRate*Re)) (fun i -> Su*amplitude - Su*amplitude/(sampleRate*Re) * float i)
 
             let amp = Array.concat [|Attack; Decay; Sustain; Release|]
             amp
 
         let Amp = enveloppe 2 0.1 0.1 0.5 0.1
 
-        let sinus = Array.init limit (fun i -> Amp[i] * sin((Frequency/ Pi) * float i))
+        let sinus = Array.init limit (fun i -> Amp[i] * sin((freq/ Pi) * float i))
 
         Amp |> Chart.Line |> Chart.Show
 
