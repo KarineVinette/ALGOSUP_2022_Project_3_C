bca2d586360c5bb7bf8ba4f52ce990bf250a8ab8
Nicolas MIDA <nicolas.mida@algosup.com>
2022-01-26 16:55:24 +0100

    Update Waves.fs

--- Synthe/Audio/Waves.fs
+++ Synthe/Audio/Waves.fs
@@ -8,12 +8,14 @@ module Waves
     let sample x = (x + 1.)/2. * 255. |> byte
     let Pi = Math.PI
     let amp = 1.
-    let freq = NotesValues A 4
+    let freq  = NotesValues A 4 // Note are from A to G 
     let limit = 44100
     let sampleRate = 44100.
+    let noteArray = [|NotesValues C 4; NotesValues D 4; NotesValues E 4; NotesValues F 4; NotesValues G 4; NotesValues A 4; NotesValues B 4; NotesValues C 6; NotesValues C 1; |]
 
-    let sinbyte = Array.init limit (fun i -> amp * sin((2. * freq * Pi * float i)/sampleRate) |> sample)
+    let sinbyte freq amp = Array.init limit (fun i -> amp * sin((2. * freq * Pi * float i)/sampleRate) |> sample)
     let sinWave =  Array.init limit (fun i -> amp * sin((2. * freq * Pi * float i)/sampleRate))
+
 
     let triangleWave = Array.init limit (fun i ->  (2.*amp/Pi)* Math.Asin(sin((2. * freq * Pi * float i)/sampleRate)))
     let trianglebyte = Array.init limit (fun i ->  (2.*amp/Pi)* Math.Asin(sin((2. * freq * Pi * float i)/sampleRate)) |> sample)
@@ -23,3 +25,13 @@ module Waves
 
     let sawWave = Array.init limit (fun i -> (2.*amp/Pi)* Math.Atan(tan((2.*Pi*float i)/freq)))
     let sawbyte = Array.init limit (fun i -> (2.*amp/Pi)* Math.Atan(tan((2.*Pi*float i)/freq)) |> sample)
+
+
+
+    let test (nom:float [] ) =  // The most unbelievable thing to play a song
+        let mutable wave2 = sinbyte 0. 0.
+        for j in 0..noteArray.Length-1 do
+            let n = Array.init limit (fun i -> amp * sin((2. * nom.[j] * Pi * float i)/sampleRate) |> sample)
+            wave2 <- Array.concat[|wave2; n|]
+        wave2
+
--- Synthe/Visual/Charts.fs
+++ Synthe/Visual/Charts.fs
@@ -1,30 +1,30 @@
 namespace Synthe
 
 open XPlot.Plotly
-open XPlot.GoogleCharts
+// open XPlot.GoogleCharts
 open Waves
 open BasicFilters
     
 module Charts =
   // 4 basic waves
-  sinWave |> Chart.Line |> Chart.Show
-  squareWave |> Chart.Line |> Chart.Show
-  sawWave |> Chart.Line |> Chart.Show
-  triangleWave |> Chart.Line |> Chart.Show
+    sinWave |> Chart.Line |> Chart.Show
+    squareWave |> Chart.Line |> Chart.Show
+    sawWave |> Chart.Line |> Chart.Show
+    triangleWave |> Chart.Line |> Chart.Show
 
   //filters
-  byFixedAmount.newwave |> Chart.Line |> Chart.Show
+  // byFixedAmount.newwave |> Chart.Line |> Chart.Show
 
-  overdrive.sin |> Chart.Line |> Chart.Show
+  // overdrive.sin |> Chart.Line |> Chart.Show
 
-  enveloppe.Amp |> Chart.Line |> Chart.Show
+  // enveloppe.Amp |> Chart.Line |> Chart.Show
 
-  Flange.Flange(sinWave) |> Chart.Line  |> Chart.Show
+  // Flange.Flange(sinWave) |> Chart.Line  |> Chart.Show
 
-  Chords.addWaves |> Chart.Line |> Chart.Show
+  // Chords.addWaves |> Chart.Line |> Chart.Show
 
 
   // sinWave |> Chart.Line |> Chart.WithOptions(Options(curveType = "function")) |> Chart.Show
-  // triangle |> Chart.Line |> Chart.WithOptions(Options(curveType = "function")) |> Chart.Show
+  // triangleWave |> Chart.Line |> Chart.WithOptions(Options(curveType = "function")) |> Chart.Show
   // squareWave |> Chart.Line |> Chart.WithOptions(Options(curveType = "function")) |> Chart.Show
   // sawWave |> Chart.Line |> Chart.WithOptions(Options(curveType = "function")) |> Chart.Show
