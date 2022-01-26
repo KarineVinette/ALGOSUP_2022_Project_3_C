fe1ea3ee78225ab9c7dc4217766a85ee89243a0c
Nicolas MIDA <nicolas.mida@algosup.com>
2022-01-26 17:32:56 +0100

    Update Charts.fs

--- Synthe/Visual/Charts.fs
+++ Synthe/Visual/Charts.fs
@@ -23,7 +23,7 @@ module Charts =
 
   // Chords.addWaves |> Chart.Line |> Chart.Show
 
-  LowFrequencyOscillator.AM sinWave 8. 0.1 |> Chart.Line |> Chart.Show
+  // LowFrequencyOscillator.AM sinWave 8. 0.1 |> Chart.Line |> Chart.Show
 
 
   // sinWave |> Chart.Line |> Chart.WithOptions(Options(curveType = "function")) |> Chart.Show
