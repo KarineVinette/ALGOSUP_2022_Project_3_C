fc75c81ec46bca72074c6d0e2e9d89761635be6b
Rasputin-6 <78964703+BrendonDesvaux@users.noreply.github.com>
2022-01-28 16:39:13 +0100

    Update program.fs
    Added entry points for the library
    Modified charts to make them readable

---
+++ Synthe/Program.fs
@@ -0,0 +1,39 @@
+module test
+
+open Song
+open Charts
+open System
+open System.IO
+
+[<EntryPoint>]
+let main argv = 
+    //args["F", "White", "5"]
+    for arg in argv do
+        match arg with 
+        | "GoldenWind" 
+            ->  GoldenWind |> ignore ; printfn "0"
+        | "Solvieg" 
+            -> Solvieg |> ignore ; printfn "1"
+        | "SinWave"
+            -> sinVisu "a" |> ignore ; printfn "a"
+        | "SqWave"
+            -> SquareVisu "b" |> ignore ; printfn "b"
+        | "SawWave"
+            -> sawVisu "c" |> ignore ; printfn "c"
+        | "TriangleWave"
+            -> triVisu "d" |> ignore ; printfn "d"
+        | "ByFixedAMP"
+            -> ampByFixedAmound "e" |> ignore ; printfn "e"
+        | "Overdrive"
+            -> Overdrived "f" |> ignore ; printfn "f"
+        | "Envelope"
+            -> Enveloped "g" |> ignore ; printfn "g"
+        | "Flange"
+            -> Flanged "h" |> ignore ; printfn "h"
+        | "Chords"
+            -> CombinedWaves "i" |> ignore ; printfn "i"
+        | "Echo"
+            -> Echoed "j" |> ignore ; printfn "j"    
+        |_ 
+            -> printfn "Unknown command"  |> ignore
+    0
--- Synthe/Visual/Charts.fs
+++ Synthe/Visual/Charts.fs
@@ -1,32 +1,121 @@
 module Charts 
 
-  open XPlot.Plotly
-  open Waves
-  open BasicFilters
+open XPlot.GoogleCharts
+open Waves
+open BasicFilters
+
+// 4 basic waves
+let sinVisu title = 
+  let options =
+    Options
+      ( 
+      title = "Sinusoidal Wave", 
+      curveType = "function",
+      vAxis = Axis(title = "Amplitude"),
+      hAxis = Axis(title = "Frequency")
+      )
+  sinWave |> Chart.Line |> Chart.WithOptions options |> Chart.Show
+
+let SquareVisu title = 
+
+  let options =
+    Options
+      ( 
+      title = "Square Wave",
+      vAxis = Axis(title = "Amplitude"),
+      hAxis = Axis(title = "Frequency")
+      )
+  squareWave |> Chart.Line |> Chart.WithOptions options  |> Chart.Show
+
+let sawVisu title = 
+  let options =
+        Options
+          (
+          title = "Sawtooth Wave",
+          vAxis = Axis(title = "Amplitude"),
+          hAxis = Axis(title = "Frequency")
+          )
+  sawWave |> Chart.Line |> Chart.WithOptions options |> Chart.Show
+
+let triVisu title = 
+  let options =
+          Options
+            (
+            title = "Sawtooth Wave",
+            vAxis = Axis(title = "Amplitude"),
+            hAxis = Axis(title = "Frequency")
+            )
+  triangleWave |> Chart.Line |> Chart.WithOptions options |> Chart.Show
+
+//filters
+let ampByFixedAmound title=
+  let options =
+          Options
+            (
+            title = " Modified amp by a fixed amount Sin Wave",
+            vAxis = Axis(title = "Amplitude"),
+            hAxis = Axis(title = "Frequency")
+            )
+  byFixedAmount.byFixedAmount 3. sinWave |> Chart.Line |> Chart.WithOptions options |> Chart.Show
+
+let Overdrived title=
+  let options =
+          Options
+            (
+            title = " Overdrived Sin Wave",
+            vAxis = Axis(title = "Amplitude"),
+            hAxis = Axis(title = "Frequency")
+            )
+  overdrive.overdrive 0.6 sinWave 44100 |> Chart.Line |> Chart.WithOptions options |> Chart.Show
+
+let Enveloped title=
+  let options =
+          Options
+            (
+            title = " Enveloped Sin Wave",
+            vAxis = Axis(title = "Amplitude"),
+            hAxis = Axis(title = "Frequency")
+            )
+  enveloppe.enveloppe 1. 2. 0.1 0.1 0.5 0.1 |> Chart.Line |> Chart.WithOptions options |> Chart.Show
   
-  // 4 basic waves
-  // sinWave |> Chart.Line |> Chart.Show
-  // squareWave |> Chart.Line |> Chart.Show
-  // sawWave |> Chart.Line |> Chart.Show
-  // triangleWave |> Chart.Line |> Chart.Show
+let Flanged title=
+  let options =
+          Options
+            (
+            title = " Flanged Sin Wave",
+            vAxis = Axis(title = "Amplitude"),
+            hAxis = Axis(title = "Frequency")
+            )
 
-  //filters
-  // byFixedAmount.byFixedAmount 3 sinWave |> Chart.Line |> Chart.Show
+  Flange.Flange(sinWave) |> Chart.Line  |> Chart.WithOptions options |> Chart.Show
 
-  //overdrive.overdrive 0.6 sinWave 44100 |> Chart.Line |> Chart.Show
+let CombinedWaves title=
+  let options =
+          Options
+            (
+            title = " Combined Waves Sin Wave",
+            vAxis = Axis(title = "Amplitude"),
+            hAxis = Axis(title = "Frequency")
+            )
 
-  // enveloppe.enveloppe 1 2 0.1 0.1 0.5 0.1 |> Chart.Line |> Chart.Show
+  Chords.addWaves |> Chart.Line |> Chart.WithOptions options |> Chart.Show
+  
+let Echoed title=
+  let options =
+          Options
+            (
+            title = " Echoed Sin Wave",
+            vAxis = Axis(title = "Amplitude"),
+            hAxis = Axis(title = "Frequency")
+            )
 
-  //Flange.Flange(sinWave) |> Chart.Line  |> Chart.Show
+  echo.echo sinWave 0.5 0.1 |> Chart.Line |> Chart.WithOptions options |> Chart.Show 
 
-  // Chords.addWaves |> Chart.Line |> Chart.Show
-  // echo.echo sinWave 0.5 0.1 |> Chart.Line |> Chart.Show 
+// porteuse |> Chart.Line |> Chart.WithOptions(Options(curveType = "function"))|> Chart.Show
+// modulante |> Chart.Line |> Chart.WithOptions(Options(curveType = "function"))|> Chart.Show
+// LowFrequencyOscillator.AM porteuse modulante |> Chart.Line |> Chart.WithOptions(Options(curveType = "function"))|> Chart.Show
 
-  // porteuse |> Chart.Line |> Chart.WithOptions(Options(curveType = "function"))|> Chart.Show
-  // modulante |> Chart.Line |> Chart.WithOptions(Options(curveType = "function"))|> Chart.Show
-  // LowFrequencyOscillator.AM porteuse modulante |> Chart.Line |> Chart.WithOptions(Options(curveType = "function"))|> Chart.Show
-
-  // sinWave |> Chart.Line |> Chart.WithOptions(Options(curveType = "function")) |> Chart.Show
-  // triangleWave |> Chart.Line |> Chart.WithOptions(Options(curveType = "function")) |> Chart.Show
-  // squareWave |> Chart.Line |> Chart.WithOptions(Options(curveType = "function")) |> Chart.Show
-  // sawWave |> Chart.Line |> Chart.WithOptions(Options(curveType = "function")) |> Chart.Show
+// sinWave |> Chart.Line |> Chart.WithOptions(Options(curveType = "function")) |> Chart.Show
+// triangleWave |> Chart.Line |> Chart.WithOptions(Options(curveType = "function")) |> Chart.Show
+// squareWave |> Chart.Line |> Chart.WithOptions(Options(curveType = "function")) |> Chart.Show
+// sawWave |> Chart.Line |> Chart.WithOptions(Options(curveType = "function")) |> Chart.Show
