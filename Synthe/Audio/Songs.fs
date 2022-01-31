33e2444125ee4e6dc2966c0829a870e47caf2a79
Rasputin-6 <78964703+BrendonDesvaux@users.noreply.github.com>
2022-01-31 13:41:11 +0100

    Update Songs.fs
    remove duplicata

--- Synthe/Audio/Songs.fs
+++ Synthe/Audio/Songs.fs
@@ -47,80 +47,6 @@ module Song =
         ADiese HalfNote 5;
         B White 5;
         |]
-    let LVER = [|
-        D PBlack 6
-        CDiese HalfNote 6
-        B PHalfNote 5
-        A QuarterNote 5
-        FDiese PHalfNote 5
-        D QuarterNote 6
-        CDiese PBlack 6
-        B HalfNote 5
-        A PHalfNote 5
-        FDiese QuarterNote 5
-        D PHalfNote 5
-        CDiese QuarterNote 6
-        B Black 5
-        A Black 5
-        B Black 5
-        CDiese Black 6
-        D Black 6
-        B Black 5
-        A HalfNote 5
-        FDiese HalfNote 5
-        FDiese HalfNote 5
-        E HalfNote 5
-        D HalfNote 5
-        CDiese HalfNote 5
-        D HalfNote 5
-        E HalfNote 5
-        FDiese HalfNote 5
-        G HalfNote 5
-        G HalfNote 5
-        FDiese HalfNote 5
-        E HalfNote 5
-        DDiese HalfNote 5
-        E HalfNote 5
-        FDiese HalfNote 5
-        G HalfNote 5
-        A HalfNote 5
-        A HalfNote 5
-        G HalfNote 5
-        FDiese HalfNote 5
-        E HalfNote 5
-        FDiese HalfNote 5
-        G HalfNote 5
-        A HalfNote 5
-        A HalfNote 5
-        A HalfNote 5
-        A HalfNote 5
-        A HalfNote 5
-        A HalfNote 5
-        FDiese HalfNote 5
-        GDiese HalfNote 5
-        A White 5
-        D PBlack 6
-        CDiese HalfNote 6
-        B PHalfNote 5
-        A QuarterNote 5
-        FDiese PHalfNote 5
-        D QuarterNote 6
-        CDiese PBlack 6
-        B HalfNote 5
-        A PHalfNote 5
-        FDiese QuarterNote 5
-        D PHalfNote 5
-        CDiese QuarterNote 6
-        B PBlack 5
-        A HalfNote 5
-        FDiese PHalfNote 5
-        CDiese QuarterNote 5
-        D PHalfNote 5
-        CDiese QuarterNote 6
-        B White 5
-        A White 5
-        Sil Silence 0
-        |]
 
     let Lac = [|
         G Black 5
@@ -211,82 +137,82 @@ module Song =
 
     |]
 
-    // let Solvieg =[|
-    //     E White 5
-    //     C HalfNote 5
-    //     E HalfNote 5
-    //     A HalfNote 5
-    //     B HalfNote 5
-    //     B HalfNote 5
-    //     A HalfNote 5
-    //     G Round 5
-    //     G White 5
-    //     E HalfNote 5
-    //     G HalfNote 5
-    //     C HalfNote 6
-    //     D HalfNote 6
-    //     B Black 5
-    //     E White 6
-    //     E HalfNote 6
-    //     HalfSil HalfSilence 0 
-    //     B Black 4
-    //     E White 5
-    //     E HalfNote 5
-    //     Sil Silence 0
-    //     MSil MSilence 0
-    //     |]
+    let Solvieg =[|
+        E White 5
+        C HalfNote 5
+        E HalfNote 5
+        A HalfNote 5
+        B HalfNote 5
+        B HalfNote 5
+        A HalfNote 5
+        G Round 5
+        G White 5
+        E HalfNote 5
+        G HalfNote 5
+        C HalfNote 6
+        D HalfNote 6
+        B Black 5
+        E White 6
+        E HalfNote 6
+        HalfSil HalfSilence 0 
+        B Black 4
+        E White 5
+        E HalfNote 5
+        Sil Silence 0
+        MSil MSilence 0
+        |]
 
-    // let Gadjet = [|
-    //     G QuarterNote 5
-    //     ADiese QuarterNote 5
-    //     B QuarterNote 5
-    //     CDiese QuarterNote 6
-    //     DDiese HalfNote 6
-    //     B HalfNote 5
-    //     D HalfNote 6
-    //     ADiese HalfNote 5
-    //     CDiese HalfNote 6
-    //     B HalfNote 5
-    //     G QuarterNote 5
-    //     ADiese QuarterNote 5
-    //     B QuarterNote 5
-    //     CDiese QuarterNote 6
-    //     DDiese HalfNote 6
-    //     GDiese HalfNote 6
-    //     G White 6
-    //     Silence HalfNote 
-    //     E QuarterNote 6
-    //     B QuarterNote 5
-    //     B QuarterNote 5
-    //     B QuarterNote 5
-    //     E HalfNote 6
-    //     B HalfNote 5
-    //     D HalfNote 6
-    //     ADiese HalfNote 5
-    //     CDiese HalfNote 6
-    //     B HalfNote 5
-    //     Silence Round 
-    //     G QuarterNote 5
-    //     ADiese QuarterNote 5
-    //     B QuarterNote 5
-    //     CDiese QuarterNote 6
-    //     DDiese HalfNote 6
-    //     B HalfNote 5
-    //     D HalfNote 6
-    //     ADiese HalfNote 5
-    //     CDiese HalfNote 6
-    //     B HalfNote 5
-    //     G QuarterNote 5
-    //     ADiese QuarterNote 5
-    //     B QuarterNote 5
-    //     CDiese QuarterNote 6
-    //     DDiese HalfNote 6
-    //     G HalfNote 6
-    //     G White 6
-    //     MSil Round 0 
-    //     |]
+    let Gadjet = [|
+        G QuarterNote 5
+        ADiese QuarterNote 5
+        B QuarterNote 5
+        CDiese QuarterNote 6
+        DDiese HalfNote 6
+        B HalfNote 5
+        D HalfNote 6
+        ADiese HalfNote 5
+        CDiese HalfNote 6
+        B HalfNote 5
+        G QuarterNote 5
+        ADiese QuarterNote 5
+        B QuarterNote 5
+        CDiese QuarterNote 6
+        DDiese HalfNote 6
+        GDiese HalfNote 6
+        G White 6
+        Sil Silence 0 
+        E QuarterNote 6
+        B QuarterNote 5
+        B QuarterNote 5
+        B QuarterNote 5
+        E HalfNote 6
+        B HalfNote 5
+        D HalfNote 6
+        ADiese HalfNote 5
+        CDiese HalfNote 6
+        B HalfNote 5
+        Sil Silence 0 
+        G QuarterNote 5
+        ADiese QuarterNote 5
+        B QuarterNote 5
+        CDiese QuarterNote 6
+        DDiese HalfNote 6
+        B HalfNote 5
+        D HalfNote 6
+        ADiese HalfNote 5
+        CDiese HalfNote 6
+        B HalfNote 5
+        G QuarterNote 5
+        ADiese QuarterNote 5
+        B QuarterNote 5
+        CDiese QuarterNote 6
+        DDiese HalfNote 6
+        G HalfNote 6
+        G White 6
+        MSil Round 0 
+        |]
         
-    // beginning of Unravel song 
+    //beginning of Unravel song 
     let Unravel = [|
         ADiese HalfNote 5;
         C Black 6;
