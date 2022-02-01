b6c6060f2af36c3daba2d3d45639210b00101d72
Rasputin-6 <78964703+BrendonDesvaux@users.noreply.github.com>
2022-02-01 12:23:18 +0100

    Update test.fs
    updated unit tests and removed useless and spaghetti code

--- Synthe.Tests/UnitTest1.fs
+++ Synthe.Tests/UnitTest1.fs
@@ -1,28 +1,28 @@
-﻿module Synthe.tests
+﻿namespace SyntheAudio
 
 open NUnit.Framework
+open Waves
 
+// module test =
+//     [<SetUp>]
+//     let Setup () = ()
 
-[<SetUp>]
-let Setup () =
-    ()
+//     [<Test>]
+//     let sineWave () =
+//         let result = sinWave.Length
+//         Assert.AreEqual(limit, result)
 
-// [<Test>]
-// let sineWave() =
-//    let result = sinWave.Length
-//    Assert.AreEqual(120000, result)
-// [<Test>]
-// let triangle() =
-//    let result = triangleWave.Length
-//    Assert.AreEqual(120000, result)
+//     [<Test>]
+//     let triangle () =
+//         let result = triangleWave.Length
+//         Assert.AreEqual(120000, result)
 
-// [<Test>]
-// let squarre() =
-//    let result = squareWave.Length
-//    Assert.AreEqual(120000, result)
+//     [<Test>]
+//     let squarre () =
+//         let result = squareWave.Length
+//         Assert.AreEqual(limit, result)
 
-// [<Test>]
-// let Sawtooth() =
-//    let result = sawWave.Length
-//    Assert.AreEqual(120000, result)
-   
+//     [<Test>]
+//     let Sawtooth () =
+//         let result = sawWave.Length
+//         Assert.AreEqual(120000, result)
