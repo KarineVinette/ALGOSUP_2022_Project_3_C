0cda90d8a3607357f47391395345522f7325309b
Brendon Desvaux <78964703+BrendonDesvaux@users.noreply.github.com>
2022-02-07 14:43:20 +0100

    Update Program.fs
    reworked user friendly

--- Synthe/Program.fs
+++ Synthe/Program.fs
@@ -11,47 +11,76 @@ open Charts
 open Url
 
 module Program =
-    let searchMusic (name: string) =
+    let rec searchMusic (name: string) =
         match name with
-        | "GoldenWind" -> GoldenWind
-        | "InTheEnd" -> InTheEnd
-        | "Lac" -> Lac
-        | "Solvieg" -> Solvieg
-        | "Gadjet" -> Gadjet
-        | "Unravel" -> Unravel
+        | "goldenWind" -> GoldenWind
+        | "inTheEnd" -> InTheEnd
+        | "lac" -> Lac
+        | "solvieg" -> Solvieg
+        | "gadjet" -> Gadjet
+        | "unravel" -> Unravel
         | "LVER" -> LVER
-        | "Kalinka" -> Kalinka
+        | "kalinka" -> Kalinka
         | "RR" -> RR
-        | "Sus" -> Amogus
-        | "Megalovania" -> Megalovania
+        | "sus" -> Amogus
+        | "megalovania" -> Megalovania
         | "TOM" -> TOM
-        | "ZeldaOverworld" -> ZeldaOverworld
-        | "Crazy" -> Crazy
-        | _ -> raise (System.Exception("The song name is not valid / or doesn't exist, please try again"))
+        | "zeldaOverworld" -> ZeldaOverworld
+        | "crazy" -> Crazy
+        | "song" ->  
+                printfn
+                    " 
+                    List of songs:
+                    - goldenWind            Play the original theme from Jojo's Bizare Adventure
+                    - solvieg               Play 'Solvieg song' from Peer Gynt
+                    - RR                    Play 'Rick Roll, Never Gonna Give You Up'
+                    - megalovania           Play the original theme from the game 'Undertale'
+                    - zelda                 Play the original theme from the game 'Zelda'
+                    - lac                   Play 'Les Lacs du Connemara' de 'Michel Sardou'
+                    - unravel               Play the original theme from 'Unravel'
+                    - kalinka               Play the traditional russian song 'Kalinka'
+                    - TOM                   Play 'A-ha, Take On Me'
+                    - LVER                  Play 'La Vie En Rose' from 'Edith Piaf'
+                    - amogus                Play 'Body discovered' sound from the game 'Among Us'
+                    - crazy                 Play 'CrazyFrog' from 'Alex F'
+                    - numb                  Play 'Numb' from 'Linkin Park'
+                    - inTheEnd              Play 'In The End' from 'Linkin Park'
+                    - lettreAElise          Play 'Lettre A Elise' from 'Bethoveen'
+
+
+                    "
+                Console.Write("Please enter a song >")
+                let message = Console.ReadLine()
+                searchMusic message 
+        | _ -> 
+                printfn "Unknown song, try typing 'song'" 
+                Console.Write("Please enter a song >")
+                let message = Console.ReadLine()
+                searchMusic message
     
 
     let rec commands (name : string) =
         match name with
-        | "Mogus"-> 
+        | "mogus"-> 
             PlaySong(Amogus)
-            Console.Write("Please enter a command:")
+            Console.Write("Please enter a command >")
             let message = Console.ReadLine()
             commands message |> ignore 
-        | "PlayFile"-> 
+        | "playFile"-> 
                 Console.Write("Enter a valid path to your file: ")
                 let url = Console.ReadLine()
                 PlayURL url 
-                Console.Write("Please enter a command:")
+                Console.Write("Please enter a command >")
                 let message = Console.ReadLine()
                 commands message |> ignore 
-        | "PlaySong"-> 
+        | "playSong"-> 
                 Console.Write("Enter your music name : ")
                 let name = Console.ReadLine()
                 PlaySong(searchMusic (name)) 
-                Console.Write("Please enter a command:")
+                Console.Write("Please enter a command >")
                 let message = Console.ReadLine()
                 commands message |> ignore 
-        | "SaveFile"-> 
+        | "saveFile"-> 
                 Console.Write("Enter your filename to save : ")
                 let file = Console.ReadLine()
 
@@ -59,83 +88,83 @@ module Program =
                     File.Create($"./Audio/CreatedSounds/{file}.wav")
 
                 CreateWavFile.write stream (SongAssemble(searchMusic (file)))
-                Console.Write("Please enter a command:")
+                Console.Write("Please enter a command >")
                 let message = Console.ReadLine()
                 commands message |> ignore 
                 
-        | "SinWave"-> 
+        | "sinWave"-> 
             sinVisu()
-            Console.Write("Please enter a command:")
+            Console.Write("Please enter a command >")
             let message = Console.ReadLine()
             commands message |> ignore 
-        | "SquareWave"-> 
+        | "squareWave"-> 
             SquareVisu()
-            Console.Write("Please enter a command:")
+            Console.Write("Please enter a command >")
             let message = Console.ReadLine()
             commands message |> ignore 
-        | "SawWave"->
+        | "sawWave"->
             sawVisu()
-            Console.Write("Please enter a command:")
+            Console.Write("Please enter a command >")
             let message = Console.ReadLine()
             commands message |> ignore 
-        | "TriangleWave"-> 
+        | "triangleWave"-> 
             triVisu()
-            Console.Write("Please enter a command:")
+            Console.Write("Please enter a command >")
             let message = Console.ReadLine()
             commands message |> ignore
-        | "ByFixedAMP"-> 
+        | "byFixedAMP"-> 
             ampByFixedAmound()
-            Console.Write("Please enter a command:")
+            Console.Write("Please enter a command >")
             let message = Console.ReadLine()
             commands message |> ignore
-        | "Overdrive"-> 
+        | "overdrive"-> 
             Overdrived()
-            Console.Write("Please enter a command:")
+            Console.Write("Please enter a command >")
             let message = Console.ReadLine()
             commands message |> ignore
-        | "Envelope"-> 
+        | "envelope"-> 
             Enveloped()
-            Console.Write("Please enter a command:")
+            Console.Write("Please enter a command >")
             let message = Console.ReadLine()
             commands message |> ignore
-        | "Flange"-> 
+        | "flange"-> 
             Flanged()
-            Console.Write("Please enter a command:")
+            Console.Write("Please enter a command >")
             let message = Console.ReadLine()
             commands message |> ignore
-        | "Chords"-> 
+        | "chords"-> 
             CombinedWaves()
-            Console.Write("Please enter a command:")
+            Console.Write("Please enter a command >")
             let message = Console.ReadLine()
             commands message |> ignore
-        | "Echo"-> 
+        | "echo"-> 
             Echoed()
-            Console.Write("Please enter a command:")
+            Console.Write("Please enter a command >")
             let message = Console.ReadLine()
             commands message |> ignore
-        | "Reverb"-> 
+        | "reverb"-> 
             Reverbed()
-            Console.Write("Please enter a command:")
+            Console.Write("Please enter a command >")
             let message = Console.ReadLine()
             commands message |> ignore
         | "AM"-> 
             AM()
-            Console.Write("Please enter a command:")
+            Console.Write("Please enter a command >")
             let message = Console.ReadLine()
             commands message |> ignore
         | "FM"-> 
             FM()
-            Console.Write("Please enter a command:")
+            Console.Write("Please enter a command >")
             let message = Console.ReadLine()
             commands message |> ignore
-        | "HighPass"-> 
+        | "highPass"-> 
             HighPass()
-            Console.Write("Please enter a command:")
+            Console.Write("Please enter a command >")
             let message = Console.ReadLine()
             commands message |> ignore
-        | "LowPass" -> 
+        | "lowPass" -> 
             LowPass()
-            Console.Write("Please enter a command:")
+            Console.Write("Please enter a command >")
             let message = Console.ReadLine()
             commands message |> ignore   
         | "help" ->
@@ -144,26 +173,28 @@ module Program =
                     List of commands:
                     - help                  Show all the listed commands
                     - songs                 List all the songs you can play using 'dotnet run PlaySong'
-                    - PlaySong              Play selected song from the 'Song's list
-                    - PlayFile              Play a selected file
-                    - SaveFile              Save file on the disk in './Audio/CreatedSounds/SavedSound.wav'
-                    - SinWave               Visualise a SinWave using GoogleCharts
-                    - SqWave                Visualise a SquareWave using GoogleCharts
-                    - SawWave               Visualise a SawToothWave using GoogleCharts
-                    - Sus                   He does seems really sus...
-                    - TriangleWave          Visualise a TriangleWave using GoogleCharts
-                    - ByFixedAmount         Visualise a SinWave with amp modified by a fixed amount
-                    - Overdrive             Visualise an overdriven sound
-                    - Envelope              Visualise a SinWavew with an Envelope (ADSR)
-                    - Flange                Visualise a Flanged sound
-                    - Chords                Visualise two waves combined to get a Chords
-                    - Echo                  Visualise a echoed sound
-                    - Reverb                Visualise a reverbed sound
+                    - playSong              Play selected song from the 'Song's list
+                    - playFile              Play a selected file
+                    - saveFile              Save file on the disk in './Audio/CreatedSounds/SavedSound.wav'
+                    - sinWave               Visualise a SinWave using GoogleCharts
+                    - sqWave                Visualise a SquareWave using GoogleCharts
+                    - sawWave               Visualise a SawToothWave using GoogleCharts
+                    - sus                   He does seems really sus...
+                    - triangleWave          Visualise a TriangleWave using GoogleCharts
+                    - byFixedAmount         Visualise a SinWave with amp modified by a fixed amount
+                    - overdrive             Visualise an overdriven sound
+                    - envelope              Visualise a SinWavew with an Envelope (ADSR)    
+                    - flange                Visualise a Flanged sound
+                    - chords                Visualise two waves combined to get a Chords
+                    - echo                  Visualise a echoed sound
+                    - reverb                Visualise a reverbed sound
                     - AM                    Visualise a SinWave using with amplitude modulation
                     - FM                    Visualise a SinWave with frequency modulation
+                    - lowPass             Visualise Low Pass filter 
+                    - highPass            Visualise High Pass filter 
 
                     "
-                Console.Write("Please enter a command:")
+                Console.Write("Please enter a command >")
                 let message = Console.ReadLine()
                 commands message |> ignore
         | "songs" ->
@@ -172,28 +203,28 @@ module Program =
                     You may enter a song's name after the message 'Enter your music name' appears.
 
                     List of songs:
-                    - GoldenWind            Play the original theme from Jojo's Bizare Adventures part 5
-                    - Solvieg               Play 'Solvieg song' from Peer Gynt
+                    - goldenWind            Play the original theme from Jojo's Bizare Adventures part 5
+                    - solvieg               Play 'Solvieg song' from Peer Gynt
                     - RR                    Play 'Rick Roll, Never Gonna Give You Up'
-                    - Megalovania           Play the original theme from the game 'Undertale'
-                    - Zelda                 Play the original theme from the game 'Zelda'
-                    - Lac                   Play 'Les Lacs du Connemara' de 'Michel Sardou'
-                    - Unravel               Play the original theme from 'Unravel'
-                    - Kalinka               Play the traditional russian song 'Kalinka' from 'Ivan Larionov'
+                    - megalovania           Play the original theme from the game 'Undertale'
+                    - zelda                 Play the original theme from the game 'Zelda'
+                    - lac                   Play 'Les Lacs du Connemara' de 'Michel Sardou'
+                    - unravel               Play the original theme from 'Unravel'
+                    - kalinka               Play the traditional russian song 'Kalinka' from 'Ivan Larionov'
                     - TOM                   Play 'A-ha, Take On Me'
                     - LVER                  Play 'La Vie En Rose' from 'Edith Piaf'
-                    - Amogus                Play 'Body discovered' sound from the game 'Among Us'
-                    - Crazy                 Play 'CrazyFrog' from 'Alex F'
-                    - Numb                  Play 'Numb' from 'Linkin Park'
-                    - InTheEnd              Play 'In The End' from 'Linkin Park'
-                    - LettreAElise          Play 'Lettre A Elise' from 'Bethoveen'
+                    - amogus                Play 'Body discovered' sound from the game 'Among Us'
+                    - crazy                 Play 'CrazyFrog' from 'Alex F'
+                    - numb                  Play 'Numb' from 'Linkin Park'
+                    - inTheEnd              Play 'In The End' from 'Linkin Park'
+                    - lettreAElise          Play 'Lettre A Elise' from 'Bethoveen'
 
 
                     "
-                Console.Write("Please enter a command:")
+                Console.Write("Please enter a command >")
                 let message = Console.ReadLine()
                 commands message |> ignore
-        | "Sus" ->
+        | "sus" ->
             Mogus |> ignore
 
             printfn
@@ -231,13 +262,13 @@ module Program =
 ⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⣿⣦⣤⣤⣤⣤⣾⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠻⢿⣿⣿⣿⣿⣿⣿⠿⠋⠉⠛⠋⠉⠉⠁⠀⠀⠀⠀
             "
-            Console.Write("Please enter a command:")
+            Console.Write("Please enter a command >")
             let message = Console.ReadLine()
             commands message |> ignore
         | "quit" ->  raise (System.Exception("You ended the session"))
         | _ -> 
             printfn "Unknown command, try typing 'help'" 
-            Console.Write("Please enter a command:")
+            Console.Write("Please enter a command >")
             let message = Console.ReadLine()
             commands message |> ignore
 
@@ -248,7 +279,7 @@ module Program =
         for arg in argv do
             match arg with
             | "start" -> 
-                Console.Write("Welcome to Sound Synthesis, Please enter a command:")
+                Console.Write("Welcome to Sound Synthesis, Please enter a command >")
                 let message = Console.ReadLine()
                 commands message |> ignore
                 
